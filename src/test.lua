e48f183e078aee67a094d3d3344038ca2c481bec
Nícolas Perez <nicolau.sobreira@gmail.com>
2024-11-30 12:57:31 -0300

    On terminal test

--- src/test.lua
+++ src/test.lua
@@ -1,12 +1,25 @@
 local function eraseTerminal()
 	os.execute("clear")
 end
+
+local function getTerminalSize()
+	local handle = io.popen("stty size", r)
+	local result = handle:read("*a")
+	handle:close()
+
+	if result then
+		local rows, cols = result:match("(%d+) (%d+)")
+		return tonumber(rows), tonumber(cols)
+	end
+end
+
 -- writes an `*' at column `x' , row `y'
 local function mark(x, y)
 	io.write(string.format("\27[%d;%dH*", math.floor(y), math.floor(x)))
 end
 -- Terminal size
-TermSize = { w = 80, h = 24 }
+local t_rows, t_cols = getTerminalSize()
+TermSize = { w = t_cols, h = t_rows }
 
 -- plot a function
 -- (assume that domain and image are in the range [-1,1])
