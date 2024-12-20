87b0dbafd4f925522250dcd18dde922b769f08db
Nícolas Perez <nicolau.sobreira@gmail.com>
2024-12-20 13:16:06 -0300

    Add documentation

--- src/modules/matrix.lua
+++ src/modules/matrix.lua
@@ -2,10 +2,12 @@ local tables = require("modules.tables")
 require("constants.cellsize")
 require("constants.directions")
 
+---@alias matrix table<number, table<number>>
 local M = {}
 
 ---@param width number
 ---@param height number
+---@return matrix
 function M.new(width, height)
 	local matrix = {}
 	for i = 1, height do
@@ -17,6 +19,8 @@ function M.new(width, height)
 	return matrix
 end
 
+---@param matrix matrix
+---@param sep string
 function M.print(matrix, sep)
 	sep = sep or " "
 	for i = 1, #matrix do
@@ -28,6 +32,7 @@ function M.print(matrix, sep)
 	print()
 end
 
+---@param matrix matrix
 function M.transpose(matrix)
 	local tmp
 	for i = 1, #matrix do
@@ -39,12 +44,15 @@ function M.transpose(matrix)
 	end
 end
 
+---@param matrix matrix
 function M.reverseLine(matrix)
 	for i, _ in pairs(matrix) do
 		tables.reverseT(matrix[i])
 	end
 end
 
+---@param matrix matrix
+---@param value number
 function M.set(matrix, value)
 	for i = 1, #matrix do
 		for j = 1, #matrix[i] do
@@ -53,6 +61,12 @@ function M.set(matrix, value)
 	end
 end
 
+---@param matrix1 matrix
+---@param matrix2 matrix
+---@param x2 number
+---@param y2 number
+---@param value number value to detect overlap (default is 0)
+---@return boolean if matrix 1 in in matrix 2
 function M.isOverlapping(matrix1, matrix2, x2, y2, value)
 	value = value or 0
 	for i = 1, #matrix1 do
@@ -65,6 +79,8 @@ function M.isOverlapping(matrix1, matrix2, x2, y2, value)
 	return false
 end
 
+---@param direction number
+---@param matrix matrix
 function M.rotate(direction, matrix)
 	if direction == CLOCKWISE then
 		M.transpose(matrix)
@@ -75,18 +91,10 @@ function M.rotate(direction, matrix)
 	end
 end
 
-function M.setLine(matrix, line, value)
-	for j = 1, #matrix[line] do
-		matrix[line][j] = value
-	end
-end
-
-function M.setColumn(matrix, column, value)
-	for i = 1, #matrix do
-		matrix[i][column] = value
-	end
-end
-
+---@param matrix1 matrix to be merge
+---@param matrix2 matrix will be merged
+---@param x number x to merge in matrix 1
+---@param y number y to merge in matrix 1
 function M.merge(matrix1, matrix2, x, y)
 	x = x or 0
 	y = y or 0
